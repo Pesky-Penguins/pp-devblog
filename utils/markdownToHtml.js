be652fcc39e270e4af075aca544ad9fe81d85040
NFP <nfp@pesky-penguins.com>
2022-08-23 12:02:01 -0400

    Add ability to post embeds in blog posts

--- utils/markdownToHtml.js
+++ utils/markdownToHtml.js
@@ -1,8 +1,20 @@
 import { remark } from 'remark';
-import html from 'remark-html';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeDocument from 'rehype-document';
+import rehypeFormat from 'rehype-format';
+import rehypeStringify from 'rehype-stringify';
 
 export default async function markdownToHtml(markdown, videos) {
-  const result = await remark().use(html, true).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeDocument)
+    .use(rehypeFormat)
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .process(markdown);
+
   const complete = Object.entries(videos).reduce((acc, [path, placeholder]) => {
     if (!path || path === 'undefined') {
       return acc;
